ccb425843f2b5ce3a4e0896c2b8eb1f538f1e9ee
ihor-0406 <sevcenkoi461@gmail.com>
2025-07-20 00:08:17 +0200

    Fix build and tests

--- src/main/java/org/example/travelapp/controller/FrontendController.java
+++ src/main/java/org/example/travelapp/controller/FrontendController.java
@@ -2,20 +2,16 @@ package org.example.travelapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class FrontendController {
 
-    @GetMapping(value = {"/{path:[^\\.]*}", "/", "/**/{path:[^\\.]*}"})
+    @GetMapping({"/", "/**"})
     public String forward() {
         return "forward:/index.html";
     }
 }
-
-
 
 
