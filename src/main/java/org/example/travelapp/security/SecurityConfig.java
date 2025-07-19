6ee4f8e9f3b1fd432862b0692287fd19fbe7fe49
ihor-0406 <sevcenkoi461@gmail.com>
2025-07-19 09:09:28 +0200

    Disable anonymous user in SecurityConfig

--- src/main/java/org/example/travelapp/security/SecurityConfig.java
+++ src/main/java/org/example/travelapp/security/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AnonymousConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -62,6 +63,7 @@ public class SecurityConfig {
                                 "/api/profile/**").authenticated()
                         .anyRequest().permitAll() //
                 )
+                .anonymous(AnonymousConfigurer::disable)
                 .exceptionHandling(ex -> ex.accessDeniedHandler(accessDeniedHandler))
                 .oauth2Login(oauth2 -> oauth2
                                 .loginPage("/login")
