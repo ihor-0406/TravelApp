c0be570884d1aab27852852d9a267e5bbe7f0bc9
ihor-0406 <sevcenkoi461@gmail.com>
2025-07-21 15:18:53 +0200

    final

--- frontend/src/pages/EnterEmail.jsx
+++ frontend/src/pages/EnterEmail.jsx
@@ -20,27 +20,31 @@ export default function EnterEmail() {
       navigate('/profile');
     } catch (err) {
       if (err.response?.status === 409) {
-        setError('Пользователь с таким email уже существует');
+        setError('A user with this email already exists');
       } else {
-        setError('Ошибка при сохранении email');
+        setError('Failed to save email');
       }
     }
   };
 
   return (
-    <div className="container mt-5">
-      <h2>Укажите email для завершения входа</h2>
+    <div className="container mt-5  ">
+        <div className='d-flex justify-content-between flex-column align-items-center'>
+            <h2>Please enter your email to complete login</h2>
       <form onSubmit={handleSubmit}>
         <input
           type="email"
-          className="form-control mb-2"
-          placeholder="Введите email"
+          className="form-control my-4 px-5 rounded-pill"
+          placeholder="Enter your email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
         />
-        <button className="btn btn-success">Подтвердить</button>
-      </form>
+       
+      </form> 
+      <button className="btn btn-outline-dark rounded-pill ">Confirm</button>
+        </div>
+      
       {error && <p className="text-danger mt-2">{error}</p>}
     </div>
   );
